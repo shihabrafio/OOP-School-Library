821da49b5134eb3a0f0a8540a4d9fa4d01e7fe05
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-25 02:28:10 +0600

    Change the File name Person to person

--- Person.rb
+++ Person.rb
@@ -1,24 +1,21 @@
 class Person
-    attr_reader :name
-    attr_writer :name
-    attr_reader :age
-    attr_writer :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(name:"Unknown", age:, parent_permission: true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        of_age? || parent_permission 
-    end
+  def can_use_services?
+    of_age? || parent_permission
+  end
 
-    private
+  private
 
-    def of_age?
-        @age>=18
-    end
+  def of_age?
+    @age >= 18
+  end
 end
