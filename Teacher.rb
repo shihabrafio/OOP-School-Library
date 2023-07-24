863acd769a3e63f353822286f09b9b9aa8c55e1e
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-25 02:28:44 +0600

    Change the File name Teacher to teacher

--- Teacher.rb
+++ Teacher.rb
@@ -1,12 +1,12 @@
 require_relative 'Person'
 
 class Teacher < Person
-    def initialize(name:"Unknown", age, parent_permission: true, specialization)
-        super(name: name, parent_permission: parent_permission, age: age)
-        @specialization = specialization
-    end
-    
-    def can_use_services?
-        true
-    end
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
+    super(name: name, parent_permission: parent_permission, age: age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
