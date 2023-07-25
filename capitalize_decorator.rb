e0afbdedd5a61e2eac7ae906b9b7f56b9569f3f7
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-25 10:25:34 +0600

    Fix syntax Error

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,6 +2,6 @@ require_relative 'base_decorator'
 
 class Capitalize_Decorator < Base_Decorator
     def correct_name
-        @nameable.correct_name.captalize
+        @nameable.correct_name.capitalize
     end
 end
