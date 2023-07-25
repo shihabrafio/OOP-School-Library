2e7eb2d2732624f0766908107661055a408bc894
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-25 10:26:26 +0600

    Fix syntax

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'Person'
 
 class Teacher < Person
-  def initialize(age, specialization, name: 'Unknown', parent_permission: true)
+  def initialize(age, specialization, name= 'Unknown', parent_permission: true)
     super(name, parent_permission, age)
     @specialization = specialization
   end
