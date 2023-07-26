68002f5019bdf9a60d49e06a6e035f9811deb240
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-26 13:27:52 +0600

    Add  method to add student

--- student.rb
+++ student.rb
@@ -6,9 +6,13 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, parent_permission, age)
     @classroom = classroom
-    classroom.students << self
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+  
   def play_hooky
     '¯(ツ)/¯'
   end
