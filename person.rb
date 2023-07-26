cd65cc69a3525cffd98b7dd5e41810531094ba1b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-26 12:13:55 +0600

    Update person class

--- person.rb
+++ person.rb
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
@@ -26,6 +27,10 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 end
 
