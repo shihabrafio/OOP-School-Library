ca1b040649bf44524d8445869fc08ae963de51ee
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-08-01 09:42:11 +0000

    Add write_data

---
+++ preserve_data.rb
@@ -0,0 +1,21 @@
+module PreserveData
+    def write_data
+        books={}
+        people={}
+        rentals={}
+        @books.each {|book| books[book.class]= {"title"=> book.title, "author"=> book.author}}
+        @people.each do |person|
+            people[person.class] = if person.instance_of?(Teacher)
+                                     { "age" => person.age, 'name' => person.name, 'specialization' => person.specialization,
+                                       'id' => person.id }
+                                   else
+                                     { 'age' => person.age, 'name' => person.name, 
+                                       'parent_permission' => person.parent_permission, 'id' => person.id }
+      
+                                   end
+        end
+        File.write('./datas/books.json', Json.generate(books), mode:'w')
+        File.write('./datas/people.json', Json.generate(people), mode:'w')
+        File.write('./datas/rentals.json', Json.generate(rentals), mode:'w')
+    end
+end
