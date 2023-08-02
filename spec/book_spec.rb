c2558270430980d8888a97c53fd3052dae7ba4c0
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-02 16:15:38 +0600

    Fix Linter Error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,15 +5,15 @@ describe Book do
     @book = Book.new('Title', 'Author')
   end
 
-  describe "#title" do
-    it "returns the correct title" do
-      expect(@book.title).to eql "Title"
+  describe '#title' do
+    it 'returns the correct title' do
+      expect(@book.title).to eql 'Title'
     end
   end
 
-  describe "#author" do
-    it "returns the correct author" do
-      expect(@book.author).to eql "Author"
+  describe '#author' do
+    it 'returns the correct author' do
+      expect(@book.author).to eql 'Author'
     end
   end
 end
