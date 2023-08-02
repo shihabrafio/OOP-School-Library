48d62c45e60fa95390eb7c56264ba43bb02e56fe
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-02 14:05:32 +0600

    Fix Linter Error 3

--- rental_handler.rb
+++ rental_handler.rb
@@ -33,6 +33,6 @@ module RentalModule
   end
 
   def save_rentals_data(books, people, rentals)
-    @data_preserver.save_rentals(books, people,rentals)
+    @data_preserver.save_rentals(books, people, rentals)
   end
 end
