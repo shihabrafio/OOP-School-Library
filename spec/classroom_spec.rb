916d855ed6203c21191f76800705e371d178adbb
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-08-02 19:53:05 +0100

    Create unit test for Teacher class

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../classes/classroom'
+require_relative '../classes/student'
+
+describe Classroom do
+  before :each do
+    @classroom = Classroom.new('Phesique')
+  end
+
+  context 'Test creating new Classroom' do
+    it 'test retrieving label from Classroom class' do
+      expect(@classroom.label).to eq("Phesique")
+    end
+  end
+
+  context 'Test Classroom methods' do
+    students = ["student1"]
+    it 'Should return an array of added students' do
+      student1 = Student.new(23, "student1", false)
+      @classroom.add_student(student1)
+      expect(@classroom.students).to eq([student1])
+    end
+  end
+end
