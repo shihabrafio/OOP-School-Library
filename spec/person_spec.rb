282b1510c5d03c6ddea1dd7cedaa279936206586
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-02 16:15:01 +0600

    Add person testing

---
+++ spec/person_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../classes/person'
+
+describe Person do
+  before :each do
+    @person = Person.new(20, 'John Doe')
+  end
+
+  describe 'Test creating new person' do
+    it 'test retriving person name' do
+      expect(@person.name).to eq('John Doe')
+    end
+    it 'test retriving person age' do
+      expect(@person.age).to eq(20)
+    end
+  end
+  describe 'Test person methods' do
+    it 'tests can_use_services? method' do
+      expect(@person.can_use_services?).to eq(true)
+    end
+    it 'tests correct_name method' do
+      expect(@person.correct_name).to eq('John Doe')
+    end
+  end
+end
